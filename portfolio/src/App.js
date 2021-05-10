28e122dde03511a51fe69c9347f543bdff00544f
jarred <jarred.caris@gmail.com>
2021-05-10 21:01:17 +0930

    header creation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar"
-
+import Header from "./components/Header"
 function App() {
   return (
+    <>
     <Navbar />
+    <Header />
+    </>
   );
 }
 
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <div className= "header-wrapper">
+            <div className= "main-info">
+                <h1>Main info page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
